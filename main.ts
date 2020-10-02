88de1162e6e272689f62b415b88192e87e0e5653
Sooraj Sannabhadti <2005sooraj@gmail.com>
2020-10-02 15:26:56 +0800

    Added timing function, display. Added ao5 function, display. Added a start screen.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function start_screen () {
     Times = []
-    ao52 = []
+    ao5 = []
     basic.showLeds(`
         # . . . .
         . . . . .
@@ -247,59 +247,68 @@ function stop_timer () {
         Timer_running = 1
         Times.push(Current_time)
         basic.showNumber(Current_time)
+        Current_time = 0
     }
 }
-function ao5 () {
+function ao5_function () {
     Times.reverse()
-    ao52[0] = Times[0]
-    ao52[1] = Times[1]
-    ao52[2] = Times[2]
-    ao52[3] = Times[3]
-    ao52[4] = Times[4]
-    ao52.reverse()
-    ao5_total = (ao52[0] + ao52[1] + ao52[2] + ao52[3] + ao52[4]) / 5
+    ao5[0] = Times[0]
+    ao5[1] = Times[1]
+    ao5[2] = Times[2]
+    ao5[3] = Times[3]
+    ao5[4] = Times[4]
+    Times.reverse()
+    ao5_total = (ao5[0] + ao5[1] + ao5[2] + ao5[3] + ao5[4]) / 5
 }
 input.onButtonPressed(Button.A, function () {
-    let start_delay_check = 0
-    Timer_running = 0
-    start_delay = 0
-    while (start_delay_check) {
-        start_delay = start_delay + 0.01
-        basic.pause(0.01)
-        if (start_delay >= 0.5) {
-        	
-        }
-    }
-    while (Timer_running == 0) {
-        timing_function()
-        stop_timer()
-    }
+    basic.showString("ao5:")
+    basic.showNumber(ao5_total)
 })
 function timing_function () {
     Current_time = Current_time + 0.01
     basic.pause(10)
 }
 input.onButtonPressed(Button.AB, function () {
-    let ao12_total = 0
-    basic.showString("ao5:")
-    basic.showNumber(ao5_total)
-    basic.showString("ao12:")
-    basic.showNumber(ao12_total)
-})
-input.onButtonPressed(Button.B, function () {
     Timer_running = 0
+    Delay()
     while (Timer_running == 0) {
         timing_function()
         stop_timer()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    let ao12_total = 0
+    basic.showString("ao12:")
+    basic.showNumber(ao12_total)
+})
+function Delay () {
+    start_delay = 0
+    start_delay_check = 0
+    while (input.buttonIsPressed(Button.AB) && start_delay_check == 0) {
+        start_delay = start_delay + 0.01
+        if (start_delay >= 0.5) {
+            start_delay_check = 1
+        } else {
+            basic.pause(0.01)
+        }
+    }
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.clearScreen()
+}
+let start_delay_check = 0
 let start_delay = 0
 let ao5_total = 0
 let Current_time = 0
 let Timer_running = 0
-let ao52: number[] = []
+let ao5: number[] = []
 let Times: number[] = []
 start_screen()
 control.inBackground(function () {
-    ao5()
+    ao5_function()
 })
