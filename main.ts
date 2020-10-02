ff5a747638ec1bebb2d6b0ef2c229c31ffb268ed
Sooraj Sannabhadti <2005sooraj@gmail.com>
2020-10-02 16:31:30 +0800

    Added average of 5 statistics. Fixed timer delay.

--- main.ts
+++ main.ts
@@ -261,45 +261,49 @@ function ao5_function () {
     ao5_total = (ao5[0] + ao5[1] + ao5[2] + ao5[3] + ao5[4]) / 5
 }
 input.onButtonPressed(Button.A, function () {
-    basic.showString("ao5:")
-    basic.showNumber(ao5_total)
-})
-function timing_function () {
-    Current_time = Current_time + 0.01
-    basic.pause(10)
-}
-input.onButtonPressed(Button.AB, function () {
     Timer_running = 0
     Delay()
     while (Timer_running == 0) {
         timing_function()
         stop_timer()
     }
+    ao5_function()
 })
+function timing_function () {
+    Current_time = Current_time + 0.01
+    basic.pause(10)
+}
 input.onButtonPressed(Button.B, function () {
     let ao12_total = 0
+    basic.showString("ao5:")
+    basic.showNumber(ao5_total)
     basic.showString("ao12:")
     basic.showNumber(ao12_total)
+    basic.clearScreen()
 })
 function Delay () {
     start_delay = 0
     start_delay_check = 0
-    while (input.buttonIsPressed(Button.AB) && start_delay_check == 0) {
-        start_delay = start_delay + 0.01
-        if (start_delay >= 0.5) {
-            start_delay_check = 1
-        } else {
-            basic.pause(0.01)
+    while (start_delay_check == 0) {
+        if (input.buttonIsPressed(Button.AB)) {
+            while (input.buttonIsPressed(Button.AB) || start_delay_check == 0) {
+                start_delay = start_delay + 0.1
+                if (start_delay > 0.5) {
+                    start_delay_check = 1
+                } else {
+                    basic.pause(0.10)
+                }
+            }
+            basic.showLeds(`
+                # . # . #
+                . # . # .
+                # . # . #
+                . # . # .
+                # . # . #
+                `)
+            basic.clearScreen()
         }
     }
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        `)
-    basic.clearScreen()
 }
 let start_delay_check = 0
 let start_delay = 0
@@ -310,5 +314,5 @@ let ao5: number[] = []
 let Times: number[] = []
 start_screen()
 control.inBackground(function () {
-    ao5_function()
+	
 })
